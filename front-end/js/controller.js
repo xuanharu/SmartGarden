01c17d492a58468f792674c8d1136aa62fd2a28d
Tên của bạn <tinhgamia@gmail.com>
2024-05-22 02:19:55 +0700

    đã thêm xong phần chạy biểu đồ còn thao tác nút

--- front-end/js/controller.js
+++ front-end/js/controller.js
@@ -1,33 +1,38 @@
+// đây là controller sẽ lấy và hiển thị dữ liệu 2 nút nhấn led và water
 console.log(12);
 setInterval(() => {
-    $.getJSON("https://io.adafruit.com/Jackson25092002/feeds/bbc-temp", function (data) {
+    $.getJSON("https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-temp", function (data) {
         console.log(data.last_value);
         document.getElementById("temp").innerHTML = data.last_value + "℃";
     });
-    $.getJSON("	https://io.adafruit.com/Jackson25092002/feeds/bbc-water", function (data) {
+    $getJSON("https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-humi", function (data) {
+        console.log(data.last_value);
+        document.getElementById("humi").innerHTML = data.last_value + "%";
+    });
+    $.getJSON("	https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-water", function (data) {
         console.log(data.last_value);
         if (data.last_value == "OPEN")
             document.getElementById("water").checked = true;
         else document.getElementById("water").checked = false;
     });
-    $.getJSON("https://io.adafruit.com/api/v2/hjilklong/feeds/bbc-led", function (data) {
+    $.getJSON("https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-led", function (data) {
         console.log(data.last_value);
         if (data.last_value == "ON")
-            document.getElementById("light").checked = true;
-        else document.getElementById("light").checked = false;
+            document.getElementById("led").checked = true;
+        else document.getElementById("led").checked = false;
     });
 }, 5000);
 
 function Led() {
     if (document.getElementById('led').checked) {
         $.ajax({
-            url: "https://io.adafruit.com/Jackson25092002/feeds/bbc-led/data",
+            url: "https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-led/data",
             dataType: "json",
             type: "post",
             headers: {
                 'Content-Type': 'application/json',
                 'Content-Type': 'application/json',
-                'X-AIO-Key': 'aio_zcpq66VhoobW2X57E1LqDxJ00fLn'
+                'X-AIO-Key': 'aio_qNNB11Z4SydjdvRelyrlC2OgqYzZ'
             },
             data: JSON.stringify({"value": "ON"}),
             processData: false,
@@ -40,13 +45,13 @@ function Led() {
         })
     } else {
         $.ajax({
-            url: "https://io.adafruit.com/api/v2/hjilklong/feeds/bbc-led/data",
+            url: "https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-led/data",
             dataType: "json",
             type: "post",
             headers: {
                 'Content-Type': 'application/json',
                 'Content-Type': 'application/json',
-                'X-AIO-Key': 'aio_zcpq66VhoobW2X57E1LqDxJ00fLn'
+                'X-AIO-Key': 'aio_qNNB11Z4SydjdvRelyrlC2OgqYzZ'
             },
             data: JSON.stringify({ "value": "OFF" }),
             processData: false,
@@ -64,13 +69,13 @@ function Led() {
 function Water() {
     if (document.getElementById('water').checked) {
         $.ajax({
-            url: "https://io.adafruit.com/Jackson25092002/feeds/bbc-water/data",
+            url: "https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-water/data",
             dataType: "json",
             type: "post",
             headers: {
                 'Content-Type': 'application/json',
                 'Content-Type': 'application/json',
-                'X-AIO-Key': 'aio_zcpq66VhoobW2X57E1LqDxJ00fLn'
+                'X-AIO-Key': 'aio_qNNB11Z4SydjdvRelyrlC2OgqYzZ'
             },
             data: JSON.stringify({"value": "ON"}),
             processData: false,
@@ -83,13 +88,13 @@ function Water() {
         })
     } else {
         $.ajax({
-            url: "https://io.adafruit.com/Jackson25092002/feeds/bbc-water/data",
+            url: "https://io.adafruit.com/api/v2/Jackson25092002/feeds/bbc-water/data",
             dataType: "json",
             type: "post",
             headers: {
                 'Content-Type': 'application/json',
                 'Content-Type': 'application/json',
-                'X-AIO-Key': 'aio_zcpq66VhoobW2X57E1LqDxJ00fLn'
+                'X-AIO-Key': 'aio_qNNB11Z4SydjdvRelyrlC2OgqYzZ'
             },
             data: JSON.stringify({ "value": "OFF" }),
             processData: false,
